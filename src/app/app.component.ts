97f65dc401f4a64f44faeb9b9aa884b53c18890f
Lama Alosaimi <lalosaimi@tahakom.com>
2020-11-11 00:02:20 +0300

    fixed the passing variable and removed the component stock

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'stock-calculator';
+
+  public stock1: number;
+  public price1: number;
+  public stock2: number;
+  public price2: number;
+  public result: number;
+
+  calculate(){
+    this.result = ((this.stock1 * this.price1) + (this.stock2 * this.price2))
+     / (this.stock1 + this.stock2);
+
+  }
+  
 }
